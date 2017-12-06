03ee456946c89d5f99991e9b57a76845d28d14c0
Tyler Collier <tyler.collier+github@devmounta.in>
2017-12-05 22:58:20 -0700

    Add axios demo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  makeWebRequest() {
+    const url = 'http://localhost:3000/api/messages';
+    axios.post(url, {
+      text: 'hello world'
+    }).then(response => {
+      // This is where we'd normally handle the response, but it's not necessary for this simple demonstration.
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Simple Axios</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <i>Wait for your instructor to provide instructions before clicking.</i>
+          <p>
+            <button onClick={this.makeWebRequest}>Click here to send a web request with Axios</button>
+          </p>
+          <p>Requests and responses (whether success or error) can be seen in the DevTools network tab</p>
+        </div>
       </div>
     );
   }
